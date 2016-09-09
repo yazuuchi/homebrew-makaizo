ac13853b40c209254bf0e519ae824599111bcbcf
Yoshiki Yazawa <yaz@honeyplanet.jp>
2016-09-10 03:24:19 +0900

    import apktool.rb

---
+++ apktool.rb
@@ -0,0 +1,25 @@
+class Apktool < Formula
+  desc "Tool for reverse engineering 3rd party, closed, binary Android apps"
+  homepage "https://github.com/iBotPeaches/Apktool"
+  url "https://bitbucket.org/iBotPeaches/apktool/downloads/apktool_2.2.0.jar", :using => :nounzip
+  sha256 "33529b641fb8985388920813d5b4e1cd75ea3d6cc0322d8489cf78a8516b5836"
+
+  bottle :unneeded
+
+  resource "sample.apk" do
+    url "https://github.com/downloads/stephanenicolas/RoboDemo/robodemo-sample-1.0.1.apk", :using => :nounzip
+    sha256 "bf3ec04631339538c8edb97ebbd5262c3962c5873a2df9022385156c775eb81f"
+  end
+
+  def install
+    libexec.install "apktool_#{version}.jar"
+    bin.write_jar_script libexec/"apktool_#{version}.jar", "apktool"
+  end
+
+  test do
+    resource("sample.apk").stage do
+      system bin/"apktool", "d", "robodemo-sample-1.0.1.apk"
+      system bin/"apktool", "b", "robodemo-sample-1.0.1"
+    end
+  end
+end
